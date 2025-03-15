2b0dfb98dec29c797c756f6103c0a2aa5d3a360a
hakeem235 <hakeemkoo@gmail.com>
2025-03-15 11:00:55 +0300

    all

---
+++ static/app.js
@@ -0,0 +1,9 @@
+const updateClock = () => {
+    const now = new Date();
+    let hours = now.getHours().toString().padStart(2, '0');
+    let minutes = now.getMinutes().toString().padStart(2, '0');
+    let seconds = now.getSeconds().toString().padStart(2, '0');
+    document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
+}
+setInterval(updateClock, 1000);
+updateClock();
